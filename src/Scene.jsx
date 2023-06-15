0133c0fc305a1e9dfa6e80aea30018eb32db9026
GameBayoumy <xilionhero@gmail.com>
2023-06-15 07:48:34 +0000

    GLTF importer

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,33 +1,50 @@
-import { XR, RayGrab, useHitTest } from "@react-three/xr"
-import { useFBX } from "@react-three/drei"
-import React from "react"
+import { XR, RayGrab, useHitTest } from "@react-three/xr";
+import { KTX2Loader } from "three/examples/jsm/loaders/KTX2Loader";
+import { useThree } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import React from "react";
 
 export function HitTest(props) {
-    const modelRef = React.useRef()
+  const modelRef = React.useRef();
 
-    // Used to move the model to the hit point
-    useHitTest((hitMatrix) => {
-        if (modelRef.current) {
-            console.log(modelRef.current.position)
-            hitMatrix.decompose(modelRef.current.position, modelRef.current.quaternion, modelRef.current.scale)
-        }
-    })
+  // Used to move the model to the hit point
+  useHitTest((hitMatrix) => {
+    if (modelRef.current) {
+      console.log(modelRef.current.position);
+      hitMatrix.decompose(
+        modelRef.current.position,
+        modelRef.current.quaternion,
+        modelRef.current.scale
+      );
+    }
+  });
 
-    return <primitive ref={modelRef} {...props} scale={0.01} />
+  return <primitive ref={modelRef} {...props} scale={0.05} />;
 }
 
 export default function ARScene() {
-    const model = useFBX('/building/build_001.fbx')
+  const gl = useThree((state) => state.gl);
+  const model = useGLTF(
+    "/death-mask/compressed/20120001-BKD-DeathMask_Low_Compressed.gltf",
+    true,
+    true,
+    (loader) => {
+      const ktx2Loader = new KTX2Loader().setTranscoderPath("/basis/");
+      ktx2Loader.detectSupport(gl);
+      loader.setKTX2Loader(ktx2Loader);
+      console.log(ktx2Loader);
+    }
+  );
 
-    return (
-        <>
-            <directionalLight intensity={1} position={[1, 2, 3]} />
-            <ambientLight intensity={0.2} />
-            <XR>
-                <RayGrab>
-                    <HitTest object={model} />
-                </RayGrab>
-            </XR>
-        </>
-    )
+  return (
+    <>
+      <directionalLight intensity={0.5} position={[1, 2, 3]} />
+      <ambientLight intensity={0.2} />
+      <XR>
+        <RayGrab>
+          <HitTest object={model.scene} />
+        </RayGrab>
+      </XR>
+    </>
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,44 @@
-import "./style.css"
-import ReactDOM from "react-dom/client"
-import { Canvas } from "@react-three/fiber"
-import { Environment, OrbitControls, Html, useProgress } from "@react-three/drei"
-import Scene from "./Scene"
-import { ARButton } from "@react-three/xr"
-import { Suspense } from "react"
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  Html,
+  useProgress
+} from "@react-three/drei";
+import Scene from "./Scene";
+import { ARButton } from "@react-three/xr";
+import { Suspense } from "react";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-    <>
-        <ARButton />
-        <Canvas>
-            <OrbitControls />
-            <Environment preset="city" background blur={0.6} />
-            <Suspense fallback={<Loader />}>
-                <Scene />
-            </Suspense>
-        </Canvas>
-    </>
-)
-
+  <>
+    <ARButton />
+    <Canvas>
+      <OrbitControls />
+      {/* <Environment preset="city" background blur={0.6} /> */}
+      <Environment background resolution={64}>
+        <Striplight position={[10, 2, 0]} scale={[1, 3, 10]} />
+        <Striplight position={[-10, 2, 0]} scale={[1, 3, 10]} />
+        <mesh scale={100}>
+          <sphereGeometry args={[1, 64, 64]} />
+          <LayerMaterial side={THREE.BackSide}>
+            <Base color="blue" alpha={1} mode="normal" />
+            <Depth colorA="#00ffff" colorB="#ff8f00" alpha={0.5} mode="normal" near={0} far={300} origin={[100, 100, 100]} />
+            <Noise mapping="local" type="cell" scale={0.5} mode="softlight" />
+          </LayerMaterial>
+        </mesh>
+      </Environment>
+      <Suspense fallback={<Loader />}>
+        <Scene />
+      </Suspense>
+    </Canvas>
+  </>
+);
 
 function Loader() {
-    const { progress } = useProgress()
-    return <Html center>{progress} % loaded</Html>
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
 }
